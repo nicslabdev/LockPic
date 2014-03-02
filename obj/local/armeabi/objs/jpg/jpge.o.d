da0f782995d6c15295c1ddb5d88e64ff8ee99a0a
carlospares <carlosparespulido@gmail.com>
2014-03-02 11:24:03 +0100

    Uploaded the source code for the app

---
+++ obj/local/armeabi/objs/jpg/jpge.o.d
@@ -0,0 +1,129 @@
+obj/local/armeabi/objs/jpg/jpge.o: jni/jpge.cpp \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/string.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/android/api-level.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/malloc.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/android/log.h \
+ jni/jpge.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdlib.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/alloca.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/strings.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdint.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/memory.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/jni.h \
+ jni/include/openssl/evp.h jni/include/openssl/opensslconf.h \
+ jni/include/openssl/ossl_typ.h jni/include/openssl/e_os2.h \
+ jni/include/openssl/symhacks.h jni/include/openssl/bio.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdio.h \
+ jni/include/openssl/crypto.h jni/include/openssl/stack.h \
+ jni/include/openssl/safestack.h jni/include/openssl/opensslv.h \
+ jni/include/openssl/objects.h jni/include/openssl/obj_mac.h \
+ jni/include/openssl/asn1.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/time.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/time.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/time.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/siginfo.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm-generic/siginfo.h \
+ jni/include/openssl/bn.h jni/include/openssl/aes.h
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/string.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/android/api-level.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/android/log.h:
+
+jni/jpge.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdlib.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/alloca.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/strings.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/types.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/types.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/types.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/memory.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/jni.h:
+
+jni/include/openssl/evp.h:
+
+jni/include/openssl/opensslconf.h:
+
+jni/include/openssl/ossl_typ.h:
+
+jni/include/openssl/e_os2.h:
+
+jni/include/openssl/symhacks.h:
+
+jni/include/openssl/bio.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdio.h:
+
+jni/include/openssl/crypto.h:
+
+jni/include/openssl/stack.h:
+
+jni/include/openssl/safestack.h:
+
+jni/include/openssl/opensslv.h:
+
+jni/include/openssl/objects.h:
+
+jni/include/openssl/obj_mac.h:
+
+jni/include/openssl/asn1.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/time.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/time.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/time.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/siginfo.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm-generic/siginfo.h:
+
+jni/include/openssl/bn.h:
+
+jni/include/openssl/aes.h:
---
+++ obj/local/armeabi/objs/jpg/stb_image.o.d
@@ -0,0 +1,152 @@
+obj/local/armeabi/objs/jpg/stb_image.o: jni/stb_image.c \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/android/log.h \
+ jni/include/openssl/evp.h jni/include/openssl/opensslconf.h \
+ jni/include/openssl/ossl_typ.h jni/include/openssl/e_os2.h \
+ jni/include/openssl/symhacks.h jni/include/openssl/bio.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdio.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/cdefs.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/android/api-level.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/types.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdint.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/_types.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/machine/_types.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/posix_types.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/stddef.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/compiler.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/posix_types.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/types.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/types.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/machine/kernel.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h \
+ jni/include/openssl/crypto.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdlib.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/string.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/malloc.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/alloca.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/strings.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/memory.h \
+ jni/include/openssl/stack.h jni/include/openssl/safestack.h \
+ jni/include/openssl/opensslv.h jni/include/openssl/objects.h \
+ jni/include/openssl/obj_mac.h jni/include/openssl/asn1.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/time.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/time.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/time.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/siginfo.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm-generic/siginfo.h \
+ jni/include/openssl/bn.h jni/include/openssl/aes.h jni/stb_image.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/math.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/limits.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/limits.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/limits.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/machine/internal_types.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/machine/limits.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/syslimits.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/page.h \
+ C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/assert.h
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/android/log.h:
+
+jni/include/openssl/evp.h:
+
+jni/include/openssl/opensslconf.h:
+
+jni/include/openssl/ossl_typ.h:
+
+jni/include/openssl/e_os2.h:
+
+jni/include/openssl/symhacks.h:
+
+jni/include/openssl/bio.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdio.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/cdefs.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/android/api-level.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/types.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdint.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/_types.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/machine/_types.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/posix_types.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/stddef.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/compiler.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/posix_types.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/types.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/types.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/machine/kernel.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/sysmacros.h:
+
+jni/include/openssl/crypto.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/stdlib.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/string.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/malloc.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/alloca.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/strings.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/memory.h:
+
+jni/include/openssl/stack.h:
+
+jni/include/openssl/safestack.h:
+
+jni/include/openssl/opensslv.h:
+
+jni/include/openssl/objects.h:
+
+jni/include/openssl/obj_mac.h:
+
+jni/include/openssl/asn1.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/time.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/time.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/time.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/siginfo.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm-generic/siginfo.h:
+
+jni/include/openssl/bn.h:
+
+jni/include/openssl/aes.h:
+
+jni/stb_image.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/math.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/limits.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/limits.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/linux/limits.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/machine/internal_types.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/machine/limits.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/sys/syslimits.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/asm/page.h:
+
+C:/android-ndk-r9/platforms/android-14/arch-arm/usr/include/assert.h:
